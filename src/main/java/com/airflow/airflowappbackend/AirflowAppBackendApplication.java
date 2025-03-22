0dadb5177639af5dea7481a58701df721de3f6b0
Dhruv-86 <dhruvrpatel86@gmail.com>
2025-03-22 13:39:45 -0500

    Initial appbackend commit

---
+++ src/main/java/com/airflow/airflowappbackend/AirflowAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.airflow.airflowappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirflowAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirflowAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/airflow/airflowappbackend/AirflowAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airflow.airflowappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirflowAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
